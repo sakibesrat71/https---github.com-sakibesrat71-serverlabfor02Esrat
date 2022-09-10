f5e962d2bc7a5b83a368c787c26a8393341c96d9
sakibesrat71 <esratebtida@iut-dhaka.edu>
2022-09-11 01:23:03 +0600

    ee

--- lab2/excercise1and2/httpd.js
+++ lab2/excercise1and2/httpd.js
@@ -4,4 +4,4 @@ http.createServer(function (req, res) {
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write("<h1>Hello World</h1>");
       return res.end();
-}).listen(8000);
+}).listen(8000);  
